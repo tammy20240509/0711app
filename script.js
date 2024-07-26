2af7329eafb96ec6b734a7b4716481ef3d2bcea8
tammy20240509 <2023dlsxedgeishizuka@gmail.com>
2024-07-26 22:56:31 +0900

    資源のデプロイ

--- script.js
+++ script.js
@@ -1,6 +1,30 @@
 var app = new Vue({
   el: '#app',
   data: {
-    framework: '選択してね',
+    framework: '下記から購入決定',
   }
 })
+function audio() { document.getElementById('btn_audio').currentTime = 0; //連続クリックに対応
+    document.getElementById('btn_audio').play(); //クリックしたら音を再生
+}
+//ここから
+  // ==========================================================================
+  // コース一覧の拡大画像モーダル処理
+  // ==========================================================================
+
+  // コース画像モーダル表示イベント
+  $(".course-item img").click(function () {
+    // まず、クリックした画像の HTML(<img>タグ全体)を#frayDisplay内にコピー
+    $("#grayDisplay").html($(this).prop("outerHTML"));
+    //そして、fadeInで表示する。
+    $("#grayDisplay").fadeIn(200);
+    return false;
+  });
+
+  // コース画像モーダル非表示イベント
+  // モーダル画像背景 または 拡大画像そのものをクリックで発火
+  $("#grayDisplay").click(function () {
+    // 非表示にする
+    $("#grayDisplay").fadeOut(200);
+    return false;
+  });
